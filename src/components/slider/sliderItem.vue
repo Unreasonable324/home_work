1b407aac332ad8253105f97a4d7a3fa34df6e942
Andrey <a8surd324@yandex.ru>
2024-09-27 20:41:55 +0700

    Update sliderItem.vue

--- src/components/slider/sliderItem.vue
+++ src/components/slider/sliderItem.vue
@@ -9,9 +9,9 @@ defineProps({
 <template>
   <a href="#" class="md:min-w-[160px] min-w-[40vw] bg-white rounded-[10px] shadow-[0_10px_10px_0_rgba(0,0,0,0.07)] flex flex-col">
     <div
-      class="w-full rounded-[10px] bg-no-repeat bg-cover"
+      class="md:min-h-[160px] min-h-[40vw] w-full rounded-[10px] bg-no-repeat bg-cover"
       :style="{ backgroundImage: `url(${item.image})` }"
-      style="aspect-ratio: 1"
+      
     ></div>
     <div class="p-4 text-primary-text flex flex-col gap-3 justify-between h-full">
       <div>{{ item.title }}</div>
